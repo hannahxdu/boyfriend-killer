671558bc5a1a2af5cad27987a954277c3994c5d8
egyajure <71519668+egyajure@users.noreply.github.com>
2022-11-12 17:38:07 -0500

    version I meant to upload, using for prototype

--- Arduino/heartbeatPlot.ino
+++ Arduino/heartbeatPlot.ino
@@ -1,11 +1,13 @@
-//basic starter code for prototype
+//basic starter code for prototype, essentially is just being used for now to show the hardware works
 
 int const PULSE_SENSOR_PIN = 0;   // 'S' Signal pin connected to A0
 
+int isLying;
 int Signal;                // Store incoming ADC data. Value can range from 0-1024
-int Threshold = 400;       // Determine which Signal to "count as a beat" and which to ignore.
+int Threshold = 330;       // Determine which Signal to "count as lying" and which to ignore.//need to work on calibrating
 
 void setup() {
+	pinMode(LED_BUILTIN,OUTPUT);  
 	Serial.begin(9600);           // Set comm speed for serial plotter window
 }
 
@@ -15,11 +17,13 @@ void loop() {
 
 	Serial.println(Signal);                // Send the signal value to serial plotter
 
-	if(Signal > Threshold){                // If the signal is above threshold, turn on the LED
-    Serial.print(Lying)
+	if(Signal > Threshold){
+    digitalWrite(LED_BUILTIN,HIGH);
+    isLying = 1;                        //function will give the value '1' if the heartbeat measured is above 400
 	} else {
-		Serial.print(Telling the truth)
+    digitalWrite(LED_BUILTIN,LOW);
+    isLying = 0;                        //function will give the value '0' if the heartbeat mesured is below 400 (not lying)
   }
-	delay(650);
+	delay(300);
 
 }
